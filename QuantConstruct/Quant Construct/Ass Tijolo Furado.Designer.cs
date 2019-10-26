dfebc8c268ca28100daa70eb8fdf1d7e9f3afecb
DhyogoAmerico <dhyogo.ags@hotmail.com>
2019-10-26 18:12:52 -0300

    Form_tijolos

---
+++ QuantConstruct/Quant Construct/Ass Tijolo Furado.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Quant_Construct
+{
+    partial class Ass_Tijolo_Furado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAjuda = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.txtCimento = new MetroFramework.Controls.MetroTextBox();
+            this.txtCal = new MetroFramework.Controls.MetroTextBox();
+            this.txtAreia = new MetroFramework.Controls.MetroTextBox();
+            this.cmbTijolo = new MetroFramework.Controls.MetroComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.txtJunta = new MetroFramework.Controls.MetroTextBox();
+            this.txtAreaP = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel9 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel10 = new MetroFramework.Controls.MetroLabel();
+            this.lblCal = new MetroFramework.Controls.MetroLabel();
+            this.lblCimento = new MetroFramework.Controls.MetroLabel();
+            this.lblAreia = new MetroFramework.Controls.MetroLabel();
+            this.lblTijolo = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmdAjuda
+            // 
+            this.cmdAjuda.Location = new System.Drawing.Point(604, 21);
+            this.cmdAjuda.Name = "cmdAjuda";
+            this.cmdAjuda.Size = new System.Drawing.Size(75, 23);
+            this.cmdAjuda.TabIndex = 0;
+            this.cmdAjuda.Text = "Ajuda";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(39, 88);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(145, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Proporção de cimento:";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(39, 117);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(114, 19);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "Proporção de cal:";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(39, 146);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(127, 19);
+            this.metroLabel3.TabIndex = 1;
+            this.metroLabel3.Text = "Proporção de areia:";
+            // 
+            // txtCimento
+            // 
+            this.txtCimento.Location = new System.Drawing.Point(190, 84);
+            this.txtCimento.Name = "txtCimento";
+            this.txtCimento.Size = new System.Drawing.Size(75, 23);
+            this.txtCimento.TabIndex = 1;
+            // 
+            // txtCal
+            // 
+            this.txtCal.Location = new System.Drawing.Point(190, 113);
+            this.txtCal.Name = "txtCal";
+            this.txtCal.Size = new System.Drawing.Size(75, 23);
+            this.txtCal.TabIndex = 2;
+            // 
+            // txtAreia
+            // 
+            this.txtAreia.Location = new System.Drawing.Point(190, 142);
+            this.txtAreia.Name = "txtAreia";
+            this.txtAreia.Size = new System.Drawing.Size(75, 23);
+            this.txtAreia.TabIndex = 3;
+            // 
+            // cmbTijolo
+            // 
+            this.cmbTijolo.FormattingEnabled = true;
+            this.cmbTijolo.ItemHeight = 23;
+            this.cmbTijolo.Items.AddRange(new object[] {
+            "Tijolo Baiano - B10/19",
+            "Tijolo Baiano - B10/29",
+            "Tijolo - B12/29",
+            "Tijolo - B15/29",
+            "Tijolo - B15/39",
+            "Tijolo Paulista - B12/14/24",
+            "Tijolo Duplo - B10/20",
+            "Tijolo Comum - C5/20"});
+            this.cmbTijolo.Location = new System.Drawing.Point(309, 154);
+            this.cmbTijolo.Name = "cmbTijolo";
+            this.cmbTijolo.Size = new System.Drawing.Size(163, 29);
+            this.cmbTijolo.TabIndex = 6;
+            this.cmbTijolo.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(505, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(39, 209);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(108, 19);
+            this.metroLabel4.TabIndex = 5;
+            this.metroLabel4.Text = "Largura da junta:";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(39, 237);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(105, 19);
+            this.metroLabel5.TabIndex = 5;
+            this.metroLabel5.Text = "Área da parede:";
+            // 
+            // txtJunta
+            // 
+            this.txtJunta.Location = new System.Drawing.Point(190, 205);
+            this.txtJunta.Name = "txtJunta";
+            this.txtJunta.Size = new System.Drawing.Size(75, 23);
+            this.txtJunta.TabIndex = 4;
+            // 
+            // txtAreaP
+            // 
+            this.txtAreaP.Location = new System.Drawing.Point(190, 233);
+            this.txtAreaP.Name = "txtAreaP";
+            this.txtAreaP.Size = new System.Drawing.Size(75, 23);
+            this.txtAreaP.TabIndex = 5;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(309, 132);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(90, 19);
+            this.metroLabel6.TabIndex = 6;
+            this.metroLabel6.Text = "Tipo de tijolo:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTijolo);
+            this.groupBox1.Controls.Add(this.lblAreia);
+            this.groupBox1.Controls.Add(this.lblCimento);
+            this.groupBox1.Controls.Add(this.lblCal);
+            this.groupBox1.Controls.Add(this.metroLabel10);
+            this.groupBox1.Controls.Add(this.metroLabel9);
+            this.groupBox1.Controls.Add(this.metroLabel8);
+            this.groupBox1.Controls.Add(this.metroLabel7);
+            this.groupBox1.Location = new System.Drawing.Point(10, 284);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(703, 156);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultado";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(29, 27);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(248, 19);
+            this.metroLabel7.TabIndex = 0;
+            this.metroLabel7.Text = "Quantidade de Cimento (sacos de 50kg):";
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.AutoSize = true;
+            this.metroLabel8.Location = new System.Drawing.Point(29, 57);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(217, 19);
+            this.metroLabel8.TabIndex = 0;
+            this.metroLabel8.Text = "Quantidade de Cal (sacos de 20kg):";
+            // 
+            // metroLabel9
+            // 
+            this.metroLabel9.AutoSize = true;
+            this.metroLabel9.Location = new System.Drawing.Point(29, 87);
+            this.metroLabel9.Name = "metroLabel9";
+            this.metroLabel9.Size = new System.Drawing.Size(166, 19);
+            this.metroLabel9.TabIndex = 0;
+            this.metroLabel9.Text = "Qauntidade de Areia (m3):";
+            // 
+            // metroLabel10
+            // 
+            this.metroLabel10.AutoSize = true;
+            this.metroLabel10.Location = new System.Drawing.Point(29, 117);
+            this.metroLabel10.Name = "metroLabel10";
+            this.metroLabel10.Size = new System.Drawing.Size(205, 19);
+            this.metroLabel10.TabIndex = 0;
+            this.metroLabel10.Text = "Quantidade de Tijolos (unidades):";
+            // 
+            // lblCal
+            // 
+            this.lblCal.AutoSize = true;
+            this.lblCal.Location = new System.Drawing.Point(252, 57);
+            this.lblCal.Name = "lblCal";
+            this.lblCal.Size = new System.Drawing.Size(16, 19);
+            this.lblCal.TabIndex = 1;
+            this.lblCal.Text = "0";
+            // 
+            // lblCimento
+            // 
+            this.lblCimento.AutoSize = true;
+            this.lblCimento.Location = new System.Drawing.Point(283, 27);
+            this.lblCimento.Name = "lblCimento";
+            this.lblCimento.Size = new System.Drawing.Size(16, 19);
+            this.lblCimento.TabIndex = 1;
+            this.lblCimento.Text = "0";
+            // 
+            // lblAreia
+            // 
+            this.lblAreia.AutoSize = true;
+            this.lblAreia.Location = new System.Drawing.Point(201, 87);
+            this.lblAreia.Name = "lblAreia";
+            this.lblAreia.Size = new System.Drawing.Size(16, 19);
+            this.lblAreia.TabIndex = 1;
+            this.lblAreia.Text = "0";
+            // 
+            // lblTijolo
+            // 
+            this.lblTijolo.AutoSize = true;
+            this.lblTijolo.Location = new System.Drawing.Point(240, 117);
+            this.lblTijolo.Name = "lblTijolo";
+            this.lblTijolo.Size = new System.Drawing.Size(16, 19);
+            this.lblTijolo.TabIndex = 1;
+            this.lblTijolo.Text = "0";
+            // 
+            // Ass_Tijolo_Furado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbTijolo);
+            this.Controls.Add(this.txtAreaP);
+            this.Controls.Add(this.txtJunta);
+            this.Controls.Add(this.txtAreia);
+            this.Controls.Add(this.txtCal);
+            this.Controls.Add(this.txtCimento);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.cmdAjuda);
+            this.Name = "Ass_Tijolo_Furado";
+            this.Text = "Assentamento de Tijolo Furado";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton cmdAjuda;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTextBox txtCimento;
+        private MetroFramework.Controls.MetroTextBox txtCal;
+        private MetroFramework.Controls.MetroTextBox txtAreia;
+        private MetroFramework.Controls.MetroComboBox cmbTijolo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTextBox txtJunta;
+        private MetroFramework.Controls.MetroTextBox txtAreaP;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroFramework.Controls.MetroLabel lblTijolo;
+        private MetroFramework.Controls.MetroLabel lblAreia;
+        private MetroFramework.Controls.MetroLabel lblCimento;
+        private MetroFramework.Controls.MetroLabel lblCal;
+        private MetroFramework.Controls.MetroLabel metroLabel10;
+        private MetroFramework.Controls.MetroLabel metroLabel9;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+    }
+}
---
+++ QuantConstruct/Quant Construct/Ass Tijolo Furado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quant_Construct
+{
+    public partial class Ass_Tijolo_Furado : MetroFramework.Forms.MetroForm
+    {
+        public Ass_Tijolo_Furado()
+        {
+            InitializeComponent();
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
