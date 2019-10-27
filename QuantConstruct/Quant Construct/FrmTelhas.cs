0122d75cab6bae93dadd7a5d0920040f5f35b470
carloseduardo9959 <50346234+carloseduardo9959@users.noreply.github.com>
2019-10-27 17:04:24 -0300

    Correções

---
+++ QuantConstruct/Quant Construct/FrmTelhas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quant_Construct
+{
+    public partial class FrmTelhas : MetroFramework.Forms.MetroForm
+    {
+        public FrmTelhas()
+        {
+            InitializeComponent();
+        }
+    }
+}
